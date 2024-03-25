16ad0818d93bdb4204623b677239c9616ad6e7ec
20nikhil02 <nikhilnayakbv55@gmail.com>
2024-03-25 16:05:14 +0530

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+
+const { app, BrowserWindow, dialog } = require('electron');
 const { autoUpdater } = require('electron-updater');
 const path = require('path');
 
@@ -18,15 +19,17 @@ function createMainWindow() {
     mainWindow.loadFile(path.join(__dirname, 'login.html'));
 }
 
+//      Hazel Releases
+/*
 app.on('ready', () => {
     createMainWindow();
 
-    autoUpdater.setFeedURL({
+    /*autoUpdater.setFeedURL({
         provider: 'github',
         owner: '20nikhil02',
         repo: 'Electron-Calci',
         private: false 
-    });
+    });*
 
     
     setInterval(() => {
@@ -53,4 +56,36 @@ app.on('ready', () => {
         console.error('There was a problem updating the application');
         console.error(message);
     });
+});*/
+
+
+
+//          GitHub Release
+app.on('ready', () => {
+    createMainWindow();
+
+    autoUpdater.setFeedURL({
+        provider: 'github',
+        owner: '20nikhil02',
+        repo: 'Electron-Calci',
+        private: true 
+    });
+
+    autoUpdater.checkForUpdatesAndNotify();
 });
+
+autoUpdater.on("update-available",()=>{
+    dialog.showMessageBox("update-available");
+})
+
+autoUpdater.on("checking-for-update",()=>{
+    dialog.showMessageBox("update-available");
+})
+
+autoUpdater.on("download-progress",()=>{
+    dialog.showMessageBox("update-available");
+})
+
+autoUpdater.on("update-downloaded",()=>{
+    dialog.showMessageBox("update-downloaded");
+})
